9ef405f9416853aa4a724b1ba25395dac8622c16
alecv <Alecvanspr@gmail.com>
2020-11-09 20:55:06 +0100

    commit met shooting enemies

--- Week 10/Program.cs
+++ Week 10/Program.cs
@@ -1,17 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;  
 
 namespace WPFW_Voorbereiding
 {
    public class Player
     {
         public int x;
-        public int lives = 3;
+        public int lives {get; set;}
         public bool living = true;
 
         public string message = " ";
 
-        public Player()
+        public Player(int lives )
         {
+            this.lives = lives;
             x = Console.WindowWidth / 2;
             message = "Lives "+ lives;
         }
@@ -45,6 +47,7 @@ namespace WPFW_Voorbereiding
         public int x;
         public int y;
 
+        public List<Bullet> bullets = new List<Bullet>();
         public Enemy() 
         {
             x = Console.WindowWidth / 2;
@@ -57,18 +60,37 @@ namespace WPFW_Voorbereiding
                 resetPosition(enemy,player);
             }
         }
-        public static void moveVerticalEnemyDown(Enemy enemy,Player player){
+        public static void moveEnemyHorizontal(Enemy enemy, Player player) {
             Random random = new Random();
-            if(enemy.y<=Console.WindowHeight){
-                enemy.y+=1; 
-                if(enemy.x<=Console.WindowWidth){
-                    enemy.x+=1;
-                }else{
-                    enemy.x=-1;
+                Action[] actions = {
+                () => { enemy.x -= 1; },
+                () => { enemy.x += 1; }
+                };
+            if(enemy.y <= Console.WindowHeight && enemy.x < Console.WindowWidth) {
+                if(enemy.x == 0) {
+                    enemy.x += 2;
                 }
-            }else{
+                if (enemy.x == Console.WindowWidth) {
+                    enemy.x -= 2; 
+                }
+                moveEnemyDown(enemy, player);
+                actions[random.Next(actions.Length)]();
+            } else {
                 resetPosition(enemy,player);
             }
+        }
+        public static void moveShooting(Enemy enemy,Player player){
+            Random random = new Random();
+            moveEnemyDown(enemy,player);
+            if(random.Next(5)==1){
+                Shoot(enemy);
+            }
+        }
+        public static void Shoot(Enemy enemy){
+            Bullet bullet = new Bullet();
+            bullet.x = enemy.x;
+            bullet.y = enemy.y;
+            enemy.bullets.Add(bullet);
         }
         public static void resetPosition(Enemy enemy,Player player){
                 Random random = new Random();
@@ -79,15 +101,20 @@ namespace WPFW_Voorbereiding
                 enemy.x=random.Next(Console.WindowWidth);
         }
     }
+    public class Bullet{
+        public int x;
+        public int y;
+    }
 
     class Program
     {
         public static int beurten = 0;
         static void Main(string[] args)
         {
-            Player player = new Player();
+            Player player = new Player(3);
             Enemy enemy = new Enemy();
             Enemy verticalEnemy = new Enemy();
+            Enemy ShootingEnemy = new Enemy();
             
             Console.Clear();
             Console.CursorVisible = false;
@@ -98,8 +125,10 @@ namespace WPFW_Voorbereiding
                 Console.Write(player.message);
                 printPlayer(player);
                 printEnemy(enemy,'E');
-                printEnemy(verticalEnemy,'v');
-                System.Threading.Thread.Sleep(75);
+                printEnemy(verticalEnemy,'V');
+                printEnemy(ShootingEnemy,'S');
+                printBullets(ShootingEnemy.bullets);
+                System.Threading.Thread.Sleep(50);
                 beurten++;
                 Console.SetCursorPosition(player.x, Console.WindowHeight);
                 Console.Write(" ");
@@ -107,6 +136,9 @@ namespace WPFW_Voorbereiding
                 Console.Write(" ");
                 Console.SetCursorPosition(verticalEnemy.x, verticalEnemy.y);
                 Console.Write(" ");
+                Console.SetCursorPosition(ShootingEnemy.x, ShootingEnemy.y);
+                Console.Write(" ");
+                clearBullets(ShootingEnemy.bullets);
                 if (Console.KeyAvailable)
                 {
                     ConsoleKey key = Console.ReadKey(true).Key;
@@ -118,24 +150,45 @@ namespace WPFW_Voorbereiding
                     if (key.Equals(ConsoleKey.D))
                     {
                         player.right();
-                        
                     }
-                    
                 }
-                if(beurten%5==0){
-                        beurten=1;
+                if(beurten%20==0){
+                    beurten=1;
+                    Enemy.moveShooting(ShootingEnemy,player);
+                }
+                if(beurten%10==0){
                         Enemy.moveEnemyDown(enemy,player);
-                        Enemy.moveVerticalEnemyDown(verticalEnemy,player);
                     }
+                if(beurten%7==0){
+                    Enemy.moveEnemyHorizontal(verticalEnemy,player);
+                }
+                printBullets(ShootingEnemy.bullets);
             }//end whileloop
         }
         public static void printPlayer(Player player){
             Console.SetCursorPosition(player.x, Console.WindowHeight);
             Console.Write("O");
         }
+        public static void clearBullets(List<Bullet> bullets){
+            for(int i = 0;i<bullets.Count;i++){
+                Console.SetCursorPosition(bullets[i].x, bullets[i].y);
+                Console.Write(" ");
+            }
+        }
         public static void printEnemy(Enemy enemy,char icon){
             Console.SetCursorPosition(enemy.x, enemy.y);
             Console.Write(icon);
+        }
+        public static void printBullets(List<Bullet> bullets){
+            for(int i = 0;i<bullets.Count;i++){
+                if(bullets[i].y!=Console.WindowHeight){
+                Console.SetCursorPosition(bullets[i].x, bullets[i].y);
+                Console.Write('|');
+                bullets[i].y+=1;
+                }else{
+                    //bullets.Remove(i);
+                }
+            }
         }
     }
 }
