91cfe1bb82fdb4bc21ed737abbc166785159afb5
Dechaun <dechaun@users.noreply.github.com>
2020-11-09 14:41:17 +0100

    VSC Project WPFW Voorbereiding

---
+++ Week 10/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WPFW_Voorbereiding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
