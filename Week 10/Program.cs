b38ddb95cf3234015056864872b9f7af87a4a249
alecv <Alecvanspr@gmail.com>
2020-11-09 17:33:22 +0100

    ding gefixed

--- Week 10/Program.cs
+++ Week 10/Program.cs
@@ -2,7 +2,7 @@
 
 namespace WPFW_Voorbereiding
 {
-    public class Player
+   public class Player
     {
         public int x;
 
@@ -23,44 +23,68 @@ namespace WPFW_Voorbereiding
     }
 
     public class Enemy {
-        private int x;
-        private int y;
+        public int x;
+        public int y;
 
         public Enemy() 
         {
             x = Console.WindowWidth / 2;
-            y = Console.WindowHeight / 2;
+            y = 0;
+        }
+            public static void moveEnemyDown(Enemy enemy){
+            if(enemy.y<=Console.WindowHeight){
+               enemy.y+=1; 
+            }else{
+                Random random = new Random();
+                enemy.y=0;
+                enemy.x=random.Next(Console.WindowWidth);
+            }
         }
     }
 
     class Program
     {
+        public static int beurten = 0;
         static void Main(string[] args)
         {
             Player player = new Player();
+            Enemy enemy = new Enemy();
+            
             Console.Clear();
             Console.CursorVisible = false;
             while (true)
             {
                 Console.SetCursorPosition(player.x, Console.WindowHeight);
                 Console.Write("O");
+                Console.SetCursorPosition(enemy.x, enemy.y);
+                Console.Write("E");
                 System.Threading.Thread.Sleep(100);
+                beurten++;
                 Console.SetCursorPosition(player.x, Console.WindowHeight);
+                Console.Write(" ");
+                Console.SetCursorPosition(enemy.x, enemy.y);
                 Console.Write(" ");
                 if (Console.KeyAvailable)
                 {
                     ConsoleKey key = Console.ReadKey(true).Key;
-                    if (key.Equals(ConsoleKey.J))
+                    if (key.Equals(ConsoleKey.A))
                     {
                         player.left();
+                        
+                        
                     }
-                    if (key.Equals(ConsoleKey.K))
+                    if (key.Equals(ConsoleKey.D))
                     {
                         player.right();
+                        
                     }
+                    
                 }
-
-            }
+                if(beurten%4==0){
+                        Enemy.moveEnemyDown(enemy);
+                    }
+            }//end whileloop
         }
     }
 }
+ 
