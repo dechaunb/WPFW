caf80eccf764365745e0a8c0f4ff462f23be2e57
alecv <Alecvanspr@gmail.com>
2020-11-10 19:49:36 +0100

    ModelErbijKrijgen

--- Week 10b/Mvc10B/Controllers/HomeController.cs
+++ Week 10b/Mvc10B/Controllers/HomeController.cs
@@ -27,6 +27,10 @@ namespace Mvc10B.Controllers
         {
             return View();
         }
+        public IActionResult GeefDeStudent()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
---
+++ Week 10b/Mvc10B/Models/Student.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Mvc10B.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set;}
+        public string Voornaam {get; set;}
+        public string Achternaam {get; set;}
+        public string Woonplaats {get; set;}
+    }
+}
