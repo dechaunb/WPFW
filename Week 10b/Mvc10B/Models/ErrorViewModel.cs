ece3a21ec66856abe824cc733404ff0f3d28e282
Scott van Duin <scott.vduin@gmail.com>
2020-11-10 15:18:10 +0100

    Made MVC + Gitignore

---
+++ Week 10b/Mvc10B/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Mvc10B.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
